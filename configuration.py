9ef39cad1fbffc0c714d95dc1c76a022bb71b58a
Alex <alexmeister1995@gmail.com>
2023-09-02 07:30:58 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://3830f8dc-4ca6-4f97-874b-5fce16c8b7d5.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
